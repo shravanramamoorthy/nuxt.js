52a4b2c94fe547ef7472f9e07ca94ea6effd1b46
shravanramamoorthy <shravan@aida.io>
2021-07-19 17:08:59 +0530

    updates

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,23 @@
 <template>
-  <Tutorial/>
+    <div>
+      <h1>Welcome to my page.</h1>
+      <input type="text" placeholder="ID bolo" v-model = "userID"> 
+      <button v-on:click = "buttonClicked()">Log my ID</button>
+    </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      userID: '',
+    }
+  },
+  methods: {
+    buttonClicked: function() {
+      this.$router.push('/users/' + this.userID)
+    }
+  }
+
+}
 </script>
