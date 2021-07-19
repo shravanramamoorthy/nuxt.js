1029d5b7e85507ee0286336582a78e7648fcf27f
shravanramamoorthy <shravan@aida.io>
2021-07-19 16:26:18 +0530

    pages added

---
+++ pages/users/_id/index.vue
@@ -0,0 +1,7 @@
+// Getting the dynamic ID through Vue route parameters
+// ".id" is used because folder name is "_id" 
+
+
+<template>
+    <h1>The user ID is {{$route.params.id}} </h1> 
+</template>
